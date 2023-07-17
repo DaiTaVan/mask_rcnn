53ba75ada6037bfd3d5a72ea7585b9bee4da5c1a
DaiTaVan <daintt15112002@gmail.com>
2023-07-17 15:24:15 +0700

    third

--- create_tfrecord.sh
+++ create_tfrecord.sh
@@ -1,7 +1,7 @@
 PYTHONPATH="research" python tools/datasets/create_coco_tf_record.py \
         --logtostderr \
         --include_masks \
-        --image_dir="data_original/test" \
+        --image_dir="preprocess/test" \
         --object_annotations_file="data_original/test_coco.json" \
-        --output_file_prefix="data_processed/test" \
+        --output_file_prefix="data_processed/tfrecord/test" \
         --num_shards=50
